9d18fcbfa4ae55caf11c79863a543885f69bbf85
Juanse <juansebastianhincapiemontes@gmail.com>
2026-06-12 10:49:06 -0500

    feat: hello world exercise (01)

---
+++ exercises/week_01/01_hello.f90
@@ -0,0 +1,6 @@
+program hello     ! All executable Fortran program needs a "program" block with a name
+    implicit none ! Always write this code line. This disables a weird and old
+                  ! fashion rule  
+    print *, "Hello Fortran 2008"   ! First argument (*) means output format and second
+                                    ! one the contnect         
+end program
