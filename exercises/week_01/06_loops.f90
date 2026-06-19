1813e41b081829521a5d4076caaed0ae8ca53268
Juanse <juansebastianhincapiemontes@gmail.com>
2026-06-19 11:02:49 -0500

    feat: equivalent to for loop exercise (06)

---
+++ exercises/week_01/06_loops.f90
@@ -0,0 +1,24 @@
+program loops_for
+    implicit none
+
+    integer :: n, i 
+    integer :: a, b, temp
+
+    write(*, '(A)', advance = 'no') "How many Fibonacci numbers? "
+    read *, n
+
+    a = 0
+    b = 1
+
+    do i = 1, n
+
+        temp = a + b
+
+        print '(I0, A, I0, A, I0)', a, " + ", b, " = ", temp
+
+        a = b
+        b = temp
+
+    end do
+
+end program loops_for
