bc3c28b46a0afdb49f1e2aab5786f6e77bcbfe33
Juanse <juansebastianhincapiemontes@gmail.com>
2026-09-02 10:10:10 +0200

    test: verify element parsing from gmsh mesh

---
+++ projects/heat2d/src/test_mesh.f90
@@ -0,0 +1,17 @@
+program test_mesh
+    use extract_mesh_info
+    implicit none
+
+    real, allocatable :: node_matrix(:,:)
+    integer, allocatable :: connectivity_matrix(:,:), boundary_matrix(:,:)
+    character(len=100) :: file_name
+    integer :: n_nodes
+
+    file_name = '../mesh/unit_square.msh'
+
+    call read_nodes(file_name, node_matrix, n_nodes)
+    call read_elements(file_name, connectivity_matrix, boundary_matrix)
+
+
+
+end program test_mesh 
